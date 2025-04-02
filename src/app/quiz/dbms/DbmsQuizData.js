dadbbd183bd2195b16dc4e270a4838cedf5fcb40
Dhanush-kd2312 <dhanushkm7494@gmail.com>
2025-04-02 23:28:36 +0530

    Add files via upload

---
+++ src/app/quiz/dbms/DbmsQuizData.js
@@ -0,0 +1,12 @@
+// AiQuizData.js
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+
+export default function DbmsQuizData({ children }) {
+  const searchParams = useSearchParams();
+  const numQuestions = parseInt(searchParams.get("numQuestions"), 10) || 5;
+  const timeLimit = parseInt(searchParams.get("timeLimit"), 10) || 150;
+
+  return children({ numQuestions, timeLimit });
+}
