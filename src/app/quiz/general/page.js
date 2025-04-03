54c59e98cc81aab6c426140ab8008731e8055aab
Dhanush-kd2312 <dhanushkm7494@gmail.com>
2025-04-03 18:50:13 +0530

    Add files via upload

---
+++ src/app/quiz/general/page.js
@@ -0,0 +1,21 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import AiQuizData from './GeneralQuizData';
+import { Suspense } from 'react'; // Import Suspense
+
+const AIQuizPage = dynamic(() => import('./GeneralQuizpage]]'), {
+  ssr: false,
+});
+
+export default function AiQuizPageWrapper() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <AiQuizData>
+        {({ numQuestions, timeLimit }) => (
+          <AIQuizPage numQuestions={numQuestions} timeLimit={timeLimit} />
+        )}
+      </AiQuizData>
+    </Suspense>
+  );
+}
